0f1ef0ff81227e73a544b789f8ef2226ae1e7b37
nizkiyVorobey <formobileprima@gmail.com>
2022-04-23 14:41:13 +0300

    edit ModifyWordFragment

--- app/src/main/java/com/example/ttanslateapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/ttanslateapp/presentation/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.ttanslateapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.ttanslateapp.R
+import com.example.ttanslateapp.presentation.modify_word.ModifyWordFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        launchModifyFragment()
     }
+
+    private fun launchModifyFragment() {
+        val fragment = ModifyWordFragment.newInstanceAdd()
+
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_modify_word_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+
 }
