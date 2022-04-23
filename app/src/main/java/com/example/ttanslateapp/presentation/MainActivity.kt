5cb2cb405cd3450a20d47a90ec44f2f0c34998d7
nizkiyVorobey <formobileprima@gmail.com>
2022-04-23 12:46:57 +0300

    add base dependency

--- app/src/main/java/com/example/ttanslateapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/ttanslateapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.ttanslateapp
+package com.example.ttanslateapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ttanslateapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
