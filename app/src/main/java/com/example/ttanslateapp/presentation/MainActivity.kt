77e7a2390e4b1a1abea5071e896239372b55244f
nizkiyVorobey <formobileprima@gmail.com>
2022-08-01 00:26:14 +0300

    add splash screen

--- app/src/main/java/com/example/ttanslateapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/ttanslateapp/presentation/MainActivity.kt
@@ -2,25 +2,17 @@ package com.example.ttanslateapp.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.forEach
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.ttanslateapp.R
-import com.example.ttanslateapp.presentation.exam.ExamKnowledgeWordsFragment
 import com.example.ttanslateapp.presentation.exam.ExamReminder
 import com.example.ttanslateapp.presentation.modify_word.ModifyWordModes
 import com.example.ttanslateapp.presentation.word_list.WordListFragmentDirections
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import timber.log.Timber
-import java.lang.ref.WeakReference
 import javax.inject.Inject
 
 
@@ -31,12 +23,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomBar: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         (application as TranslateApp).component.inject(this)
         setupNavigation()
         examReminder.setInitialReminderIfNeeded()
-
 
         // get text from selected items
         val text = intent
